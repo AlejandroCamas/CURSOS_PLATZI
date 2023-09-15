a8a4789c1d1bda9a6522b6169563b4cfe13c09b5
unknown <alejandromcamas@gmail.com>
2023-09-14 19:26:06 -0600

    archivos nuevos

---
+++ CURSOS-PROGRAMACION/PYTHON/RETO_CALCULADORA.py
@@ -0,0 +1,20 @@
+#
+Enero = input("Ventas totales de Enero: ")
+Febrero = input("Ventas totales de Febrero ")
+Marzo =  input("ventas totales de Marzo ")
+
+#QUE TIPO DE VARIABLE MANERJARA O TIPO DE VALORES
+Enero = int(Enero)
+Febrero = int(Febrero)
+Marzo =  int(Marzo)
+
+
+Totales = Enero + Febrero + Marzo 
+print("LA SUMA TOTAL ES: ", Totales)
+
+Totales = int (Totales)
+
+Promedio = Totales / 3
+
+print("El promedio es: ", Promedio)
+
---
+++ CURSOS-PROGRAMACION/PYTHON/TIPOS_DE_DATOS.py
@@ -0,0 +1,23 @@
+#string
+name = "Alejandro"
+name = "Camas"
+name = "21"
+
+print('name =>', name)
+print(type(name))
+
+
+
+#int
+age =21 
+print("age => ", age)
+print(type(age))
+#boolean
+is_single = False
+print('is_single =>', is_single)
+print(type(is_single))
+
+#intputs
+age = input('how old are you?')
+print('age =>', age)
+print(type(age))
